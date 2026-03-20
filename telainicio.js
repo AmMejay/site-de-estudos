dd89bf32c700799eff05ec9932c7a9806a86c108
BRYAN DA SILVA <bryan_da-silva@estudante.sesisenai.org.br>
2026-03-20 08:29:45 -0300

    conectou quimica com inicial

--- telainicio.js
+++ telainicio.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const botalogtela = document.getElementById('botalin')
+  const botquimica = document.getElementById("quimica")
+
+  botquimica.addEventListener("click" , () => {
+    window.location.href = "quimica.html"
+  })
 
   botalogtela.addEventListener('click', () => {
     window.location.href = 'index.html'
