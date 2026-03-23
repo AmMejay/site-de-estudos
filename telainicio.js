6a262d1e1160df0421972c27926fbe25f556bf11
AmMejay <nicolasrb08@gmail.com>
2026-03-23 16:02:20 -0300

    Delete telainicio.js

--- telainicio.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const botalogtela = document.getElementById('botalin')
-
-  botalogtela.addEventListener('click', () => {
-    window.location.href = 'index.html'
-  })
-})
